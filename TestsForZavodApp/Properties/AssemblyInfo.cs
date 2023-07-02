ea815a9fc6ec44973724009bc7988ea4eb8b0bad
blinkopec <vip.lolip@mail.ru>
2023-07-02 19:14:54 +0300

    mm

---
+++ TestsForZavodApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsForZavodApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsForZavodApp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b1fd8af-d127-48f5-8848-59a32d18176c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestsForZavodApp/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Controls;
+using ZavodApp;
+using ZavodApp.WorkerResources.Pages;
+using ZavodApp.WorkerResources.Pages.PagesForWork;
+
+namespace TestsForZavodApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ReportCard rc23 = new ReportCard()
+            {
+                idDetail = 1,
+                idWorker = 1,
+                numberOfDetails = 32,
+                date = new DateTime(23),
+                workingTime = 3
+            };
+
+
+            ZavodPracticeEntities.GetContext().ReportCard.Add(rc23);
+            ZavodPracticeEntities.GetContext().SaveChanges();
+
+            ReportCard rc = ZavodPracticeEntities.GetContext().ReportCard.Find(rc23);
+            Assert.IsNotNull(rc);
+            Assert.AreEqual(rc23.idReportCard, rc.idReportCard);
+            Assert.AreEqual(rc23.idWorker, rc.idWorker);
+            Assert.AreEqual(rc23.workingTime, rc.workingTime);
+            Assert.AreEqual(rc23.date, rc.date);
+            Assert.AreEqual(rc23.numberOfDetails, rc.numberOfDetails);
+            Assert.AreEqual(rc23.idDetail, rc.idDetail);
+        }
+    }
+}
